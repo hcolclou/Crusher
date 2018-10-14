cdea6eb4d95b7797e27da035d8d854574f248ebf
Harlan Colclough <35181393+hcolclou@users.noreply.github.com>
2018-10-13 23:55:03 -0700

    Minor fixes

--- AI.hs
+++ AI.hs
@@ -3,28 +3,34 @@ module AI where
 import CrusherBoard
 import Piece
 import Util
-import Debug.Trace
+import Control.Parallel
 
 depth = 2
+parallel = True
 
 calculatevalue :: [[Piece]] -> Piece -> Double
 calculatevalue board me
     | x == 0    = 0
-    | otherwise = (sqrt (x / (1.0 + x)))*(x / (sqrt (x * x)))
+    | sign > 0  = 1.5*(sqrt (x / (1.0 + x)))
+    | otherwise = (-1.0)*(sqrt (x / (1.0 + x)))
     where x = fromIntegral (abs ((countall board me) - (countall board (getotherplayer me))))
+          sign = (x / (sqrt (x * x)))
 
 calculateoddssingle :: [Board] -> Board -> Piece -> Piece -> Int -> Double
 calculateoddssingle visited (NewBoard board) me curr depth
-    | countall board curr == 0 = if curr == me then -1.0 else 1.0
-    | plays == [] = if curr == me then -1.0 else 1.0
+    | countall board curr == 0 = if curr == me then -1.0 else 1.5
+    | plays == [] = if curr == me then -1.0 else 1.5
     | depth == 0 = calculatevalue board me
     | otherwise = (calculateoddsmult ((NewBoard board):visited) plays me (getotherplayer curr) depth) / (fromIntegral (length plays))
     where plays = generateplayset visited (NewBoard board) curr
 
 calculateoddsmult :: [Board] -> [Board] -> Piece -> Piece -> Int -> Double
 calculateoddsmult _ [] _ _ _ = 0.0
-calculateoddsmult visited (h:t) me curr depth =
-    (calculateoddssingle visited h me curr (depth - 1)) + (calculateoddsmult visited t me curr depth)
+calculateoddsmult visited (h:t) me curr depth
+    | parallel  = par p1 (pseq p2 (p1 + p2))
+    | otherwise = calculateoddssingle visited h me curr (depth - 1) + calculateoddsmult visited t me curr depth
+    where p1 = calculateoddssingle visited h me curr (depth - 1)
+          p2 = calculateoddsmult visited t me curr depth
 
 generateplayset :: [Board] -> Board -> Piece -> [Board]
 generateplayset visited (NewBoard board) p = generateplaysethelp visited board pieces p
@@ -32,8 +38,9 @@ generateplayset visited (NewBoard board) p = generateplaysethelp visited board p
 
 generateplaysethelp :: [Board] -> [[Piece]] -> [Point] -> Piece -> [Board]
 generateplaysethelp _ _ [] _ = []
-generateplaysethelp visited board (h:t) p = (generatemoves board h ((snd (getmoves visited board h)) ++
-    (snd (getstomps visited board p h)))) ++ (generateplaysethelp visited board t p)
+generateplaysethelp visited board (h:t) p = (generatemoves board h (moves ++ stomps)) ++ (generateplaysethelp visited board t p)
+    where moves = snd (getmoves visited board h)
+          stomps = snd (getstomps visited board p h)
 
 generatemoves :: [[Piece]] -> Point -> [Point] -> [Board]
 generatemoves _ _ [] = []
